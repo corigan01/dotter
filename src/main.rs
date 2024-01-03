f46fec501e8e557a71786f18a7ec2700e28a8d3f
corigan01 <corigan01@gmail.com>
2024-01-03 13:51:22 -0600

    removing files

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,10 @@ enum Command {
         // Optional configuration tag
         config_name: Option<String>,
     },
+    Remove {
+        // Configuration to remove
+        config_name: String,
+    },
     Install {
         // Optional configuration tag to install from
         config_name: Option<String>,
@@ -24,14 +28,13 @@ enum Command {
 }
 
 const DEFAULT_CONFIG_NAME: &str = "default";
-const DOOT_NAME: &str = "Doot.toml";
 const DEFAULT_CONFIG_CONTENTS: &str = r#"
 [doot]
-name = "defualt"
+name = "example"
 authors = ["your name"]
 version = "0.0.1"
 
-[my_config]
+[config]
 location = "~/.config/my_config/config.toml"
 symlink = false
 topic = "My example config for example program!"
@@ -59,9 +62,45 @@ fn new(config_file: String) -> anyhow::Result<()> {
         bail!("Config name '{config_file}' should not include a file type, as dotter makes a directory!");
     }
 
-    let full_config_name = format!("{config_file}/{DOOT_NAME}");
+    let full_config_name = format!("./{config_file}/{config_file}.toml");
     make_new_doot(&full_config_name)?;
 
+    Ok(())
+}
+
+fn remove(config_file: String) -> anyhow::Result<()> {
+    println!("Removing Config: {config_file}");
+
+    let full_config_name = format!("./{config_file}");
+    let mut files_to_remove: Vec<String> = Vec::new();
+
+    for file in Path::new(&full_config_name).read_dir()? {
+        files_to_remove.push(file?.file_name().into_string().unwrap());
+    }
+
+    println!("Removing: {files_to_remove:?}");
+
+    loop {
+        print!("Are you sure you want to remove [y, N]: ");
+        std::io::stdout().flush()?;
+
+        let mut user_line = String::new();
+        std::io::stdin().read_line(&mut user_line)?;
+        let user_line = user_line.trim();
+
+        match user_line.to_lowercase().as_str() {
+            "y" => break,
+            "n" | "" => {
+                println!("Canceled!");
+                return Ok(());
+            }
+
+            _ => println!("Please use 'y', or 'n'!"),
+        }
+    }
+
+    println!("Deleting Files...");
+    std::fs::remove_dir_all(full_config_name)?;
     Ok(())
 }
 
@@ -79,16 +118,18 @@ fn main() -> anyhow::Result<()> {
     match command {
         Command::New { config_name } => {
             let config_name = config_name.unwrap_or(DEFAULT_CONFIG_NAME.into());
-            new(config_name)
+            new(config_name)?;
         }
+        Command::Remove { config_name } => remove(config_name)?,
         Command::Install { config_name } => {
             let config_name = config_name.unwrap_or(DEFAULT_CONFIG_NAME.into());
             install(config_name);
-            Ok(())
         }
         Command::List => {
             list();
-            Ok(())
         }
     }
+
+    println!("Done");
+    Ok(())
 }
